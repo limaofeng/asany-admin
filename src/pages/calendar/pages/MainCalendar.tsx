20573f9590eff64d5f8013d6e0bc2ef05f881160
李茂峰 <limaofeng@msn.com>
2022-01-07 03:25:42 +0800

    feat: 日历中显示事件

--- src/pages/calendar/pages/MainCalendar.tsx
+++ src/pages/calendar/pages/MainCalendar.tsx
@@ -1,11 +1,62 @@
+import { useCallback, useEffect, useRef } from 'react';
+
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import locale from '@fullcalendar/core/locales/zh-cn';
 
+import { useCalendarEventsLazyQuery } from '../hooks';
+
 import { Card, ContentWrapper } from '@/pages/Metronic/components';
 
+type SuccessCallback = (events: any[]) => void;
+
 function MainCalendar() {
+  const [loadCalendarEvents, { data }] = useCalendarEventsLazyQuery();
+  const state = useRef<{ callback?: SuccessCallback; events: Map<string, any> }>({
+    events: new Map(),
+  });
+
+  const setEvents = useCallback((events: any[]) => {
+    if (!state.current.callback) {
+      return;
+    }
+    const apendEvents = events.map((item) => ({
+      ...item,
+      ...item.datetime,
+      backgroundColor: item.color,
+    }));
+    for (const event of apendEvents) {
+      state.current.events.set(event.id, event);
+    }
+    state.current.callback(Array.from(state.current.events.values()));
+  }, []);
+
+  const handleEventSource = useCallback(
+    (
+      arg: { start: Date; end: Date; startStr: string; endStr: string; timeZone: string },
+      callback: SuccessCallback,
+    ) => {
+      state.current.callback = callback;
+      loadCalendarEvents({
+        variables: {
+          starts: arg.startStr,
+          ends: arg.endStr,
+        },
+      }).then((_d) => {
+        setEvents(_d.data?.events || []);
+      });
+    },
+    [loadCalendarEvents, setEvents],
+  );
+
+  useEffect(() => {
+    if (!data?.events) {
+      return;
+    }
+    setEvents(data.events || []);
+  }, [data?.events, setEvents]);
+
   return (
     <ContentWrapper header={false} footer={false} className="main-calendar">
       <Card>
@@ -19,9 +70,11 @@ function MainCalendar() {
               center: 'title',
               right: 'timeGridDay,timeGridWeek,dayGridMonth',
             }}
+            firstDay={0}
             navLinks={true}
             selectable={true}
             selectMirror={true}
+            events={handleEventSource as any}
           />
         </div>
       </Card>
