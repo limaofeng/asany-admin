7e16fb85ac9eb23276cb75171b2d086cac3baa1d
李茂峰 <limaofeng@msn.com>
2022-05-06 16:17:59 +0800

    fix: 使用域名

--- config/config.ts
+++ config/config.ts
@@ -4,15 +4,15 @@ import routes from './routes';
 
 export default defineConfig({
   apollo: {
-    uri: '//47.103.119.188:8000/graphql',
+    uri: '//api.asany.cn/graphql',
     logging: process.env.NODE_ENV === 'development',
   },
   app: {
     id: '6068485332c5fc853a65',
   },
   define: {
-    'process.env.STORAGE_URL': '//47.103.119.188:8000',
-    'process.env.MOBILE_URL': '//47.103.119.188:8002',
+    'process.env.STORAGE_URL': '//api.asany.cn',
+    'process.env.MOBILE_URL': '//wmf.app.asany.cn',
   },
   hash: true,
   antd: false,
