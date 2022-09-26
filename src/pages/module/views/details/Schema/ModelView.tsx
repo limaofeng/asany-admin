aa617beb81ac10a2d277a7cd3cb8fb97ca60a2d1
李茂峰 <limaofeng@msn.com>
2022-09-27 00:02:35 +0800

    feat: 实体操作菜单样式

--- src/pages/module/views/details/Schema/ModelView.tsx
+++ src/pages/module/views/details/Schema/ModelView.tsx
@@ -1,10 +1,12 @@
 import type { RouteComponentProps } from 'react-router';
+import { Icon } from '@asany/icons';
+import classnames from 'classnames';
 
 import { useModelQuery } from '../../../hooks';
 
 import ModelFieldsManagement from './components/ModelFieldsManagement';
 
-import { Card, Tabs } from '@/metronic';
+import { Button, Card, Dropdown, Menu, Tabs } from '@/metronic';
 import { ContentWrapper } from '@/layouts/components';
 import type { Model } from '@/types';
 
@@ -31,9 +33,47 @@ function ModelView(props: ModelViewProps) {
         !!model && (
           <>
             <div className="h-40px d-flex align-items-center">
-              <div className="d-flex align-items-end">
+              <div className="d-flex align-items-end position-relative">
                 <h1 className="mb-0 me-2">{model.name}</h1>
                 <span className="text-muted">#{model.code}</span>
+                <Dropdown
+                  // offset={[-32, 0]}
+                  placement="bottomLeft"
+                  overlay={
+                    <Menu
+                      dropdown
+                      rounded
+                      className="menu-state-bg-light-primary p-1 gap-1 menu-sub-dropdown menu-title-gray-700 menu-state-primary fw-semibold fs-base w-125px"
+                    >
+                      <Menu.Item
+                        icon={
+                          <div className={classnames('')}>
+                            <Icon name="Bootstrap/pencil-square" />
+                          </div>
+                        }
+                      >
+                        编辑
+                      </Menu.Item>
+                      <Menu.Item
+                        className="actions-delete"
+                        icon={
+                          <div className={classnames('')}>
+                            <Icon name="Bootstrap/trash" />
+                          </div>
+                        }
+                      >
+                        删除
+                      </Menu.Item>
+                    </Menu>
+                  }
+                >
+                  <Button
+                    variant="clean"
+                    style={{ right: -34, top: 2 }}
+                    className="position-absolute"
+                    icon={<Icon className="svg-icon-2" name="Bootstrap/three-dots" />}
+                  />
+                </Dropdown>
               </div>
             </div>
             <span className="text-muted lh-base">{model.description}</span>
