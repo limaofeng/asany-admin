0a97be466ddae6cad825abe45f18293b0a1a7af0
李茂峰 <limaofeng@msn.com>
2023-09-26 09:16:15 +0800

    feat: excel 预览及文档

--- src/pages/fples/viewers/ExcelViewer.tsx
+++ src/pages/fples/viewers/ExcelViewer.tsx
@@ -1,86 +1,54 @@
-import { useEffect } from 'react';
+import { useLayoutEffect, useMemo, useRef } from 'react';
 
-import LuckyExcel from 'luckyexcel';
-
-const luckyCss = {
-  margin: '0px',
-  padding: '0px',
-  position: 'absolute',
-  width: '100%',
-  height: '800px',
-  left: '0px',
-  top: '0px',
-};
-
-function loadRemoteExcel(excelFileURL: string) {
-  // 使用 fetch API 下载 Excel 文件
-  return fetch(excelFileURL)
-    .then(function (response) {
-      return response.arrayBuffer();
-    })
-    .then(function (data) {
-      return new Uint8Array(data);
-    });
-}
+import { Base64 } from 'js-base64';
 
 type ExcelViewerProps = {
-  excelUrl: string;
+  title: string;
+  excelUrl?: string;
   height: number;
-}
+  onPreviewRendered?: () => void;
+};
 
 function ExcelViewer(props: ExcelViewerProps) {
-  const { excelUrl, height } = props;
-  useEffect(() => {
-    loadRemoteExcel(excelUrl).then((data) => {
-      LuckyExcel.transformExcelToLucky(data, function (exportJson: any) {
-        const luckysheet = window.luckysheet;
+  const { excelUrl, title, height, onPreviewRendered } = props;
 
-        console.log('exportJson', exportJson);
+  const iframeRef = useRef<HTMLIFrameElement>(null);
 
-        luckysheet.create({
-          container: 'luckysheet',
-          plugins: ['chart'],
-          title: exportJson.info.name, // 工作簿名称
-          lang: 'zh', // 设定表格语言 国际化设置，允许设置表格的语言，支持中文("zh")和英文("en")
-          allowCopy: false, // 是否允许拷贝
-          showtoolbar: false, // 是否显示工具栏
-          showinfobar: false, // 是否显示顶部信息栏
-          showsheetbar: false, // 是否显示底部sheet页按钮
-          showstatisticBar: false, // 是否显示底部计数栏
-          sheetBottomConfig: false, // sheet页下方的添加行按钮和回到顶部按钮配置
-          allowEdit: false, // 是否允许前台编辑
-          enableAddRow: false, // 允许增加行
-          enableAddCol: false, // 允许增加列
-          userInfo: exportJson.info.creator, // 右上角的用户信息展示样式
-          showRowBar: false, // 是否显示行号区域
-          showColumnBar: false, // 是否显示列号区域
-          sheetFormulaBar: false, // 是否显示公式栏
-          enableAddBackTop: false, //返回头部按钮
-          rowHeaderWidth: 0, //纵坐标
-          columnHeaderHeight: 0, //横坐标
-          showstatisticBarConfig: {
-            count: false,
-            view: false,
-            zoom: false,
-          },
-          showsheetbarConfig: {
-            add: false, //新增sheet
-            menu: false, //sheet管理菜单
-            sheet: false, //sheet页显示
-          },
-          hook: {
-            // cellMousedown:this.cellMousedown,//绑定鼠标事件
-          },
-          forceCalculation: true, //强制计算公式
-          data: exportJson.sheets,
-        });
-      });
-    });
-  }, [excelUrl]);
+  const param = useMemo(() => {
+    const opts = {
+      copy: '0',
+      cors: '1',
+      fid: '111',
+      fname: title,
+      furl: excelUrl,
+      private: '1',
+      sign: '5555',
+      type: 'xlsx',
+    };
+    return Base64.encode(JSON.stringify(opts));
+  }, [excelUrl, title]);
+
+  useLayoutEffect(() => {
+    // 获取 <iframe> 元素
+    const iframe = iframeRef.current!;
+
+    // 添加 load 事件监听器
+    const handleLoad = () => {
+      onPreviewRendered && onPreviewRendered();
+    };
+
+    iframe.addEventListener('load', handleLoad);
+
+    return () => {
+      // 在组件卸载时移除事件监听器
+      iframe.removeEventListener('load', handleLoad);
+    };
+  }, [onPreviewRendered]);
+
 
   return (
-    <div className="excel-viewer">
-      <div id="luckysheet" style={{...luckyCss, height}} />;
+    <div className="excel-viewer" style={{ height }}>
+      <iframe ref={iframeRef} className="excel-viewer-iframe" src={`./xlsx1/index.html?param=${param}`} />
     </div>
   );
 }
