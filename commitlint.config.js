0a3fd62f3904987e6717e7c88e8bde3e5ebe7aa2
李茂峰 <limaofeng@msn.com>
2024-12-27 10:01:00 +0800

    chore: increase header max length to 200 characters in commitlint configuration

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,5 +2,6 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'body-max-line-length': [2, 'always', 1000],
+    'header-max-length': [2, 'always', 200],
   },
 };
